76c70e014fd5f17efc80d77ddf7044f71d4e53a0
Cillian Kennedy <kennedy.cillian@gmail.com>
2019-09-22 17:36:27 +0100

    Fix bug in tests

--- startup.sh
+++ startup.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-echo 'Symfony security check'
-symfony security:check --dir=api
+# echo 'Symfony security check'
+# symfony security:check --dir=api
 
-echo 'Install composer packages'
-composer install --prefer-dist -d api
+# echo 'Install composer packages'
+# composer install --prefer-dist -d api
 
-echo 'Install yarn packages and build frontend'
-yarn --cwd frontend && yarn --cwd frontend build
-
-echo 'Enter api root to run tests and start server'
-cd api
+# echo 'Install yarn packages and build frontend'
+# yarn --cwd frontend && yarn --cwd frontend build
 
 echo 'Run tests'
-./bin/phpunit tests/CalculationTests.php
+cd api && cp phpunit.xml.dist phpunit.xml && ./vendor/bin/simple-phpunit tests/CalculationTests.php
 
 echo 'Start development server'
 symfony server:start
