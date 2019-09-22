b26e88158cb7c21523f07e242d24676895fc6436
Cillian Kennedy <kennedy.cillian@gmail.com>
2019-09-22 17:22:13 +0100

    Get API docs running

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
